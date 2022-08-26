3ce0c74f728a26906837d6d7b62018edf1073132
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-08-26 22:43:09 +0530

    Set up react environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const aliasConfig = require("./alias");
+environment.config.merge(aliasConfig);
+
+//Now, you can import from apis folder without having to resolve the entire path each time.
+
+// Example:
+
+// import authAPI from "apis/auth";
+
 module.exports = environment
