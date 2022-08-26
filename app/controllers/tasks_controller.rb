82aa32168ac28d5e691cd2a858e85b915a0a0e4d
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-08-27 00:06:25 +0530

    Added tasks controller and index action

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,9 @@
+class TasksController < ApplicationController
+    # def index
+    #     # render html: "This is index action of Tasks controller"
+    #     render
+    # end
+    def index
+        @tasks = Task.all
+    end
+end
