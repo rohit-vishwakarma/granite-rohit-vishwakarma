492355f0d98afcbf5bba6c5a7846dad6e3fec0af
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-08-29 17:18:19 +0530

    Added ability to display list of tasks

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
-
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -14,14 +17,23 @@ const App = () => {
     // logger.error("Never use console.error");
   }, []);
 
+  // if (loading) {
+  //   return <h1>Loading...</h1>;
+  // }
   if (loading) {
-    return <h1>Loading...</h1>;
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
   }
+
   return (
     <Router>
       <Switch>
         <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
     </Router>
   );
