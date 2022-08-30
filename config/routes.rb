38104e0646c1b6c1689f01a69a2438dd59d4ea89
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-08-30 20:55:30 +0530

    Added show page for a task

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :tasks, only: :index, param: :slug
 
-  resources :tasks, only: %i[index create], param: :slug # %i[] for symbol elements in array
+  resources :tasks, only: %i[index create show], param: :slug # %i[] for symbol elements in array
   root "home#index"
   get "*path", to: "home#index", via: :all
 
