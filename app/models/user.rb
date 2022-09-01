9bdcd7ed2f56ce7c9446b5f40ace3054285f228e
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-09-01 13:17:29 +0530

    Added login feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   MAX_EMAIL_LENGTH = 255
 
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
   has_secure_password
-  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
+  has_secure_token :authentication_token
+
+  validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { maximum: MAX_EMAIL_LENGTH },
     format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: 6 }, if: -> { password.present? } # validate password
+  validates :password, length: { minimum: 6 }, if: -> { password.present? }
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
